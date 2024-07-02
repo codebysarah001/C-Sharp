69b7996a3ab1530df00333839ddfa47764d261b6
codebysarah001 <sarahshormam@gmail.com>
2024-07-03 02:17:07 +0300

    Updated

---
+++ Task6/Task6/Animal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task6
+{
+    // Create an interface named ISound that includes a method void MakeSound();.
+    public interface ISound
+    {
+        void MakeSound();
+
+    }
+
+    // 2. Create an abstract class named Animal that includes:
+
+    //	A property string Name { get; set; }.
+    //	A constructor that takes a string name parameter and sets the Name property.
+    //	An abstract method void Eat();.
+    //	A concrete method void Sleep() that prints out the animal's name followed by "is sleeping.".
+
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+
+            Name = name;
+        }
+
+        public abstract void Eat();
+
+        public void Sleep()
+        {
+            Console.WriteLine(Name + " is sleeping.");
+        }
+    }
+
+    //3.  Create two classes, Dog and Cat.
+
+    public class Dog : Animal
+    {
+        public Dog(string name) : base(name)
+        {
+
+        }
+
+        public override void Eat()
+        {
+
+            Console.WriteLine(Name + " is eating a dog's food");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(Name + " says : Woof!");
+        }
+    }
+
+    public class Cat : Animal, ISound
+    {
+        public Cat(string name) : base(name)
+        {
+
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine(Name + " is eating Friskies wet food.");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(Name + " says : Meaw!");
+        }
+    }
+
+
+
+}
---
+++ Task6/Task6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //o In the Main method, create instances of Dog and Cat.
+            //o Call the Eat, Sleep, and MakeSound methods on each instance   
+
+            Dog dog = new Dog("Bella");
+            Cat cat = new Cat("Elsa");
+
+            dog.Eat();
+            Console.WriteLine();
+            dog.Sleep();
+            Console.WriteLine();
+            dog.MakeSound();
+
+            Console.WriteLine();
+
+            cat.Eat();
+            Console.WriteLine();
+            cat.Sleep();
+            Console.WriteLine();
+            cat.MakeSound();
+
+
+        }
+    }
+}
