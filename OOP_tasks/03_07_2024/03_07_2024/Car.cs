641e152695691a2b5cfa4a56d10146a592eed1f3
codebysarah001 <sarahshormam@gmail.com>
2024-07-03 02:39:20 +0300

    Updated

---
+++ OOP_tasks/03_07_2024/03_07_2024/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_07_2024
+{
+    public class Car
+    {
+        string Make { get; set; }
+        int Year { get; set; }
+        string Type { get; set; }
+        double Price { get; set; }
+        string Model { get; set; }
+        int PalletNo { get; set; }
+
+        string Color { get; set; }
+
+        public Car(string make, int year, string type, double price, string model, int palletNo, string color)
+        {
+            Make = make;
+            Year = year;
+            Type = type;
+            Price = price;
+            Model = model;
+            PalletNo = palletNo;
+            Color = color;
+        }
+
+        public void DisplayInfo(string make, string year)
+        {
+            Console.WriteLine($"Make: {make}, Year: {year}");
+        }
+
+        public void DisplayInfo(string make)
+        {
+            Console.WriteLine($"Make: {make}");
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("Welcome Car");
+        }
+
+    }
+
+    public class BMW : Car
+    {
+        public BMW(string make, int year, string type, double price, string model, int palletNo, string color)
+            : base(make, year, type, price, model, palletNo, color)
+        {
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Welcome BMW");
+        }
+
+
+    }
+}
---
+++ OOP_tasks/03_07_2024/03_07_2024/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_07_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BMW myBMW = new BMW("BMW", 2021, "SUV", 50000, "X5", 5678, "Black");
+
+            myBMW.Display();
+
+        }
+    }
+}
